817308e8ad29c83367d01a273d50f06520a281a4
thKyomno18 <thkyomno18@hanovernorwichschools.org>
2019-03-21 14:18:25 +0000

    phonewords.hs

--- phonewords.hs
+++ phonewords.hs
@@ -5,6 +5,9 @@
     
     import PTfuncsyntax
     
+wordsToPhone a = numListToNum (newList a)
+newList a = [charToPhoneDigit x | x <- a, x /= ' ' && x /= ',']
+    
     and run this program in the same directory with your PFfuncsyntax.hs file.
     
     This program will ask the user to enter a 4-digit number. It will then list 
@@ -23,7 +26,29 @@
 
 -}
 
-import PTfuncsyntax
+charToPhoneDigit :: Char -> Int
+charToPhoneDigit c 
+ | c `elem` "abcABC" = 2
+ | c `elem` "defDEF" = 3
+ | c `elem` "ghiGHI" = 4
+ | c `elem` "jklJKL" = 5
+ | c `elem` "mnoMNO" = 6
+ | c `elem` "pqrsPQRS" = 7
+ | c `elem` "tuvTUV" = 8
+ | c `elem` "wxyzWXYZ" = 9
+ | otherwise = 0
+ 
+wordsToPhone :: String -> Int
+wordsToPhone a = numListToNum (newList a)
+newList a = [charToPhoneDigit x | x <- a, x /= ' ' && x /= ',']
 
+numListToNum a = (read :: String -> Int) (listInt a)
+listInt [] = ""
+listInt a = show (head a) ++ listInt (tail a)
 
-main = putStrLn "Put your program here!"
+main = do
+    putStrLn "Type a four-digit number:"
+    num <- readLn
+    dictionary <- readFile "/usr/share/dict/american-english"
+    let dict = words dictionary
+    mapM_ (putStrLn) [show x|x<-dict, wordsToPhone(x) == num]
